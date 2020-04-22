fffaddd5e5a661578097799692ef8022ae02b07b
Hervera <nkuliherve@gmail.com>
2020-04-22 16:44:25 +0200

    Designing UI

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,20 @@ import "package:flutter/material.dart";
 
 void main() {
   runApp(
-      Center(
-        child: Text(
-              "Hello Flutter",
-              textDirection: TextDirection.ltr
-          ),
-      )
+     MaterialApp(
+       title: "My Flutter App",
+       home: Scaffold(
+         appBar: AppBar(title: Text("My First App Screeen"),),
+         body: Material(
+           color: Colors.lightBlueAccent,
+           child: Center(
+             child: Text(
+                 "Hello Flutter",
+                 textDirection: TextDirection.ltr
+             ),
+           )
+       )
+     )
+  )
   );
 }
